783709ccc179a343a45e14fd2d4287938fa5dd7c
Dave O'Connor <daveoconnor3@gmail.com>
2016-08-23 19:36:02 -0400

    added hello world example
    Uses browserify, jsx, components

---
+++ hello-world/src/app.js
@@ -0,0 +1,8 @@
+var ReactDOM = require('react-dom');
+var React = require('react');
+var HelloWorld = require('./HelloWorld');
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('app')
+);
