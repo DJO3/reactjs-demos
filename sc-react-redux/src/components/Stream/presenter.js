c798c3a453e641e0ea6972fa34f80806cc940fe7
Dave O'Connor <daveoconnor3@gmail.com>
2016-09-25 22:05:38 -0400

    minor text adjustments

--- sc-react-redux/src/components/Stream/presenter.js
+++ sc-react-redux/src/components/Stream/presenter.js
@@ -3,10 +3,11 @@ import React from 'react'
 function Stream ({ tracks = [] }) {
   return (
     <div>
-      {tracks.map((track, key) => {
-        return <div className='track' key={key}>{track.title}</div>
-      })
-    }
+      {
+        tracks.map((track, key) => {
+          return <div className='track' key={key}>{track.title}</div>
+        })
+      }
     </div>
   )
 }
